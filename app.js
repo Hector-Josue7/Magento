dd91bd31b889c49f21fdbdca6af22e3fa4393439
Hector Josue <hectorardonm@gmail.com>
2019-08-15 00:05:02 -0600

    Se mejoro la base de datos y el diseño

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express")
 const bodyParser = require('body-parser')
 const app = express() 
 const router = require('./routes')
+const exphbs = require('express-handlebars');
+const multer = require('multer');
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image')); // colocaremos las imagenes que se suban en esa dirección y el single significa se van a subir de una en una
 app.use(express.static("public")); //definir una carpeta como publica para que los usuarios puedan acceder a su contenido
 app.use('/api', router)
 
--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/shop'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/magento'
+  /*si queremos integrar con facebook o twitter la aplicacion, entonces hacemos este codigo
+    
+    facebook{
+        secret:
+    }
+    */
 }
