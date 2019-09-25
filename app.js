18e301923260aacf87309e3dea74826c565c0fb5
Hector Josue <hectorardonm@gmail.com>
2019-09-24 22:05:33 -0600

    cambios 24/09/2019

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express") 
 var  bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts')
 const path = require('path');
+//------------------
 var upload = require('./routes/upload')
 var paginasEstaticas = require('./routes/paginas-estaticas-routers');
+var locked = require('./routes/locked');
+//----------------------------------------------
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(expressLayouts) 
+//--------------------------------
 app.use('/upload', upload);
 app.use('/paginaestatica',paginasEstaticas);
+//--------------------------------
 
+
+app.set('view engine', 'ejs')  
 
 app.use(express.static(path.join(__dirname, './public')));
 module.exports = app;
@@ -21,3 +30,12 @@ module.exports = app;
 // if('development' === app.get('env')) {
 //     app.use(errorHandler());
 //   }
+
+//const faker = require('faker')
+
+
+
+//app.use(express.static("public"));
+
+
+
